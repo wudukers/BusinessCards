0b90f7f5e607d66c1fe156c4f9f2c33c7c3d1b80
Mark Chang <mark23456@hotmail.com>
2015-05-23 22:16:32 +0800

    Update markchang.py

--- english/markchang.py
+++ english/markchang.py
@@ -11,6 +11,7 @@ def markchang(**my):
     """ Learning Support-Vector """
 
     assert my["github"] == "http://github.com/ckmarkoh"
+    assert my["blog"] == "http://cpmarkchang.logdown.com"
     assert my["email"] == "ckmarkoh@gmail.com"
     assert my["phone_number"] == "+886 953-679220"
     assert agilearning.VAT_ID == 24755908
